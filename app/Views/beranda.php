70b4dd567cd460dd09db05e0cffdfafa25154c1c
falvo02421 <falvo02421@gmail.com>
2020-09-15 03:29:02 +0700

    first commit

---
+++ app/Views/beranda.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project Codeigniter 4 dan Bootstrap 4</title>
+    <link rel="stylesheet" href="<?= base_url('css/bootstrap.min.css') ?>">
+    <script src="<?= base_url('js/bootstrap.min.js') ?>"></script>
+</head>
+<body>
+    <div class="jumbotron">
+        <h1>Welcome to My Site</h1>
+        <p>Ini adalah halaman demo untuk tutorial upload framework codeigniter 4 di hosting dengan menggunakan bootstrap 4.</p>
+    </div>
+</body>
+</html>
